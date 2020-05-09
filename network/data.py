165c9d6084ea11019eb58b03abe69647f74f67c7
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2020-05-09 15:37:52 +0200

    Added BERT

--- network/data.py
+++ network/data.py
@@ -3,6 +3,8 @@
 '''
 
 import torch
+import math
+from transformers import BertModel, BertTokenizer
 
 class CharacterData():
 	def __init__(self, file_path, device):
@@ -47,3 +49,52 @@ class CharacterData():
 		for i in inds:
 			str += self.ind_to_char[i]
 		return str
+
+class VecData():
+	def __init__(self, file_path, device):
+		# Raw text data
+		self.text_data = [x.strip() for x in open(file_path, encoding="utf-8").read().split('.')]
+		# BERT model
+		self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+		self.model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+		# ids (BERT input)
+		self.ids_list = self.text2ids(self.text_data)
+		# Word embeddings
+		self.vec_data = self.ids2vecs(self.ids_list)
+		# K: Number of classes (dimensions of word embedding)
+		self.K = self.vec_data.shape[2]
+
+		self.device = device
+
+	def text2ids(self, text):
+		ids = [torch.tensor([self.tokenizer.encode(t, add_special_tokens=True)]) for t in text]
+		return ids
+
+	def ids2vecs(self, ids_list):
+		# n_ids = ids.shape[1]
+		# size_batch = 512
+		# n_batch = math.ceil(n_ids/size_batch)
+		vecs = torch.empty(0, 1, 768)
+		for ids in ids_list:
+			with torch.no_grad():
+				hidden_states = self.model(ids)[2]
+			vecs_batch = torch.stack(hidden_states[:4]).sum(0).transpose(0, 1)
+			vecs = torch.cat([vecs, vecs_batch], dim=0)
+		return vecs
+
+	def vecs2text(self, vecs):
+		# Convert n*K matrix of word embeddings to text 
+
+		# n_vec_data = self.vec_data.shape[0]
+		# trimmed_vec_data = self.vec_data.narrow(0, 1, n_vec_data - 2)
+		dotted = torch.mm(self.vec_data.squeeze(1), vecs.transpose(0, 1))
+		argmax = dotted.max(0)[1]
+		ids = torch.empty(1, 0, dtype=torch.long)
+		# print(self.ids[0].shape)
+		# exit()
+		for one_id in self.ids_list:
+			ids = torch.cat([ids, one_id], dim=1)
+		ids = ids.squeeze(0)[argmax]
+		text = self.tokenizer.decode(ids)
+		return text
+
---
+++ network/rnn_bert.py
@@ -0,0 +1,108 @@
+'''
+    Entry point for training a character-level RNN.
+'''
+
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+import data
+import rnn_bert_net
+import rnn_bert_train
+from cross_entropy_one_hot import cross_entropy
+import utility
+
+'''
+    Network and synthesis parameters
+'''
+input_file_name = "data/speech.txt"
+save_file_name = "rnn_bert_save.pt"
+n_hidden = 500
+seq_length = 25
+syn_length = 500
+n_epochs = 60
+learning_rate = 0.1
+seed = random.randint(1, 10000)
+# seed = 999
+use_cuda = False
+'''
+    Note: Using the GPU is currently only beneficial for very large network
+    sizes since the batches are processed sequentially. For smaller net_rnn
+    GPU is much slower than CPU.
+'''
+if use_cuda and not torch.cuda.is_available():
+    print("No CUDA support found. Switching to GPU mode. ")
+    use_cuda = False
+if use_cuda:
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+'''
+    Create network and loss criterion
+'''
+torch.manual_seed(seed)
+random.seed(seed)
+data = data.VecData(input_file_name, device)
+criterion = cross_entropy
+net = rnn_bert_net.RNN(data.K, n_hidden, data.K)
+if use_cuda:
+    net = net.cuda()
+optimizer = optim.Adagrad(net.parameters(), lr=learning_rate)
+
+print("Input file: ")
+print("\t" + input_file_name)
+print()
+print("Parameters: ")
+print("\tHidden nodes M: ", n_hidden)
+print("\tSequence length: ", seq_length)
+print("\tLearning rate: ", learning_rate)
+print("\tNumber of epochs: ", n_epochs)
+print("\tRandom seed: ", seed)
+print("\tGPU: ", use_cuda)
+print()
+
+loss_vec, smooth_loss_vec = rnn_bert_train.train_net(net, criterion, optimizer, data, n_hidden, seq_length, n_epochs, learning_rate, device)
+
+'''
+    Save network and training data
+'''
+save_folder = 'saves'
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+module_id = "rnn_bert"
+config_text = "Baseline RNN (M=" + str(n_hidden) + ", seq_len=" + str(seq_length) + ", eta=" + str(learning_rate) + ")"
+torch.save({
+    'model_state_dict' : net.state_dict(),
+    'optimizer_state_dict' : optimizer.state_dict(),
+    'loss_vec' : loss_vec,
+    'smooth_loss_vec' : smooth_loss_vec,
+    'n_hidden' : n_hidden,
+    'K' : data.K,
+    'seq_length' : seq_length,
+    'learning_rate' : learning_rate,
+    'input_file_name' : input_file_name,
+    'config_text' : config_text,
+    'module_id' : module_id,
+},  save_folder + "/" + save_file_name)
+
+'''
+    Synthesize some text
+'''
+text = rnn_bert_train.synthesize_words(data, net, syn_length, device)
+print()
+print("Synthesized text:")
+print("\t" + text)
+print()
