95ca755fd0e75d77bd6bef46774609327c9b5b5f
Tomas Andersson <tomas3@kth.se>
2020-05-14 07:36:51 +0200

    Restructure eval

--- network/eval.py
+++ network/eval.py
@@ -29,12 +29,14 @@ import data
 import utility
 import sys
 
-syn_length = 200
-syn_beam_search = True
-beam_search_width = 30
+syn_length = 1000
+syn_beam_search = False
+beam_search_width = 6
 beam_search_sampler = 'Weighted' # 'WeightedNoReplacement', 'Weighted', 'Random' and 'Topk'
 seed = random.randint(1, 10000)
 seed = 999
+interpolation_rate_train = 0.01
+interpolation_rate_val = 0.1
 
 args = sys.argv
 if len(args) < 2:
@@ -70,7 +72,9 @@ print("Synthesized text:")
 print("\t" + text)
 print()
 print(checkpoint['config_text'])
-print(flush=True)
+
+smooth_loss_vec = utility.smoothed(loss_vec, interpolation_rate_train)
+print("Final smooth training loss: ", smooth_loss_vec[-1])
 
 '''
     Plot loss
@@ -81,13 +85,9 @@ print(flush=True)
 plt.plot(smooth_loss_vec, 'blue')
 if 'val_loss_vec' in checkpoint:
 	val_loss_vec = checkpoint['val_loss_vec']
-	smooth_val_loss_vec = []
-	smooth_val = val_loss_vec[0]
-	interpolation_rate = 0.01
+	smooth_val_loss_vec = utility.smoothed(val_loss_vec, interpolation_rate_val)
+	print("Final smooth validation loss: ", smooth_val_loss_vec[-1])
 	val_scale_factor = round(len(loss_vec) / len(val_loss_vec))
-	for val in val_loss_vec:
-		smooth_val = smooth_val * (1 - interpolation_rate) + val * interpolation_rate
-		smooth_val_loss_vec.append(smooth_val)
 	iterations_per_epoch = round(data_loader.n_samples / checkpoint['batch_size'] / checkpoint['seq_length'])
 	plt.plot(range(iterations_per_epoch, len(smooth_val_loss_vec) * val_scale_factor + iterations_per_epoch, val_scale_factor), smooth_val_loss_vec, 'orange')
 	
@@ -98,5 +98,6 @@ plt.xlabel("Iteration")
 plt.ylabel("Loss")
 plt.xlim(0, len(smooth_loss_vec))
 # Note: y-max is quite arbitrary and depends on the loss metric and data
-plt.ylim(0, 20)
+plt.ylim(0, 10)
+print(flush=True)
 plt.show()
